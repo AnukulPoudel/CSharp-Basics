f267797af13e261921cbf2120f1bfad78d4dc346
Arch_Anukul <anukulpoudel999@gmail.com>
2025-03-02 11:57:16 +0545

    folderManaged

--- obj/Debug/net9.0/firstChapter.AssemblyInfo.cs
+++ obj/Debug/net9.0/firstChapter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstChapter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3789e56b8785c3d655a71edab5109e770c5217")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstChapter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstChapter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
